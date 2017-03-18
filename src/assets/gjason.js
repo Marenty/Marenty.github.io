9b179c6b3722e5e7cff1204e8d0f66580a0c2c4e
marenty <marekruc@yahoo.pl>
2017-03-18 18:07:32 +0100

    Zmiana

---
+++ src/assets/gjason.js
@@ -0,0 +1,52 @@
+var piastowska =
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates": [ 18.585998 , 54.416067]
+        },
+        "properties": {
+            "name": "Piastowska"
+        }
+    };
+
+var brodnica =
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Point",
+            "coordinates":  [19.395662 , 53.25997]
+        },
+        "properties": {
+            "name": "Brodnica"
+        }
+    };
+
+var piastowska_brodnica =
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "LineString",
+            "coordinates": [
+                [18.585998 , 54.416067], [19.395662 , 53.25997]
+            ]
+        },
+        "properties": {
+            "name": "Z Piastowskiej do Brodnicy"
+        }
+    };
+
+var okolice =
+    {
+        "type": "Feature",
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [ [18.685998 , 54.316067],[18.485998 , 54.316067], [18.485998 , 54.516067] , [18.685998 , 54.516067],
+                 ]
+            ]
+        },
+        "properties": {
+            "name": "Moje okolice"
+        }
+    };
