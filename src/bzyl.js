5a56ed61d21c08ba3e2907bcd8216e1052a283fe
marenty <marekruc@yahoo.pl>
2017-04-22 21:56:48 +0200

    UP

---
+++ src/bzyl.js
@@ -0,0 +1,15 @@
+var data = {
+  "zajecia1":{"dzien":"Wtorek","godz":"10:oo","przedmiot":"Architektura serwis\u00f3w internetowych","grupa":"","sala":"25","typ":"laboratorium","uwagi":"gr. 1","datado":"2017-06-09"},
+  "zajecia2":{"dzien":"Wtorek","godz":"11:oo","przedmiot":"Architektura serwis\u00f3w internetowych","grupa":"","sala":"25","typ":"laboratorium","uwagi":"gr. 1","datado":"2017-06-09"},
+  "zajecia3":{"dzien":"Wtorek","godz":"12:oo","przedmiot":"Architektura serwis\u00f3w internetowych","grupa":"","sala":"25","typ":"laboratorium","uwagi":"gr.  2","datado":"2017-06-09"}  ,
+  "zajecia4":{"dzien":"Wtorek","godz":"13:oo","przedmiot":"Architektura serwis\u00f3w internetowych","grupa":"","sala":"25","typ":"laboratorium","uwagi":"gr.  2","datado":"2017-06-09"},
+  "zajecia5":  {"dzien":"Wtorek","godz":"14:oo","przedmiot":"Architektura serwis\u00f3w internetowych","grupa":"","sala":"2","typ":"wyk\u0142ad","uwagi":"","datado":"2017-06-09"},
+  "zajecia6":  {"dzien":"Wtorek","godz":"15:oo","przedmiot":"Architektura serwis\u00f3w internetowych","grupa":"","sala":"2","typ":"wyk\u0142ad","uwagi":"","datado":"2017-06-09"},
+  "zajecia7":  {"dzien":"Wtorek","godz":"16:oo","przedmiot":"Architektura serwis\u00f3w internetowych","grupa":"","sala":"25","typ":"laboratorium","uwagi":"gr. 3","datado":"2017-06-09"},
+  "zajecia8":  {"dzien":"Wtorek","godz":"17:oo","przedmiot":"Architektura serwis\u00f3w internetowych","grupa":"","sala":"25","typ":"laboratorium","uwagi":"gr. 3","datado":"2017-06-09"},
+  "zajecia9":  {"dzien":"\u015aroda","godz":"10:oo","przedmiot":"Seminarium","grupa":"","sala":"328","typ":"seminarium","uwagi":"","datado":"2017-06-09"},
+  "zajecia10":  {"dzien":"\u015aroda","godz":"11:oo","przedmiot":"Seminarium","grupa":"","sala":"328","typ":"seminarium","uwagi":"","datado":"2017-06-09"},
+  "zajecia11":  {"dzien":"\u015aroda","godz":"12:oo","przedmiot":"Technologie NoSQL","grupa":"","sala":"1","typ":"wyk\u0142ad","uwagi":"","datado":"2017-06-09"},
+  "zajecia12":  {"dzien":"\u015aroda","godz":"13:oo","przedmiot":"Technologie NoSQL","grupa":"","sala":"1","typ":"wyk\u0142ad","uwagi":"","datado":"2017-06-09"},
+  "zajecia13":  {"dzien":"\u015aroda","godz":"14:oo","przedmiot":"Technologie NoSQL","grupa":"","sala":"25","typ":"laboratorium","uwagi":"","datado":"2017-06-09"},
+  "zajecia14":  {"dzien":"\u015aroda","godz":"15:oo","przedmiot":"Technologie NoSQL","grupa":"","sala":"25","typ":"laboratorium","uwagi":"","datado":"2017-06-09"}};
